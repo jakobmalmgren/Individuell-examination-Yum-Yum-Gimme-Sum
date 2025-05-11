71c17c9a7b652156847bb2312628f8378236f304
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-05-11 21:30:09 +0200

    fixar så den funkar vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist", // Se till att byggmappen är korrekt
+//   },
+//   server: {
+//     fs: {
+//       strict: false, // Se till att filsystemet är tillåtet att nå din kod
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Bygg i dist-mappen
+  },
 });
